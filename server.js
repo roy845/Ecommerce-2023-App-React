e51a3c5a6c3ea2a975269fbdb15e3f7f4044b07f
roy845 <58545785+roy845@users.noreply.github.com>
2023-04-20 23:05:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,12 +11,17 @@ import cors from "cors";
 import credentials from "./middlewares/credentials.js";
 import corsOptions from "./config/corsOptions.js";
 import path from "path";
+import {fileURLToPath} from 'url'
 
 //configure env
 dotenv.config();
 
 //database config
 connectDB();
+
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // rest object
 const app = express();
